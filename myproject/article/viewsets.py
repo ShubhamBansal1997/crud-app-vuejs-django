1cc519e71fed40388560f9f0b39367c57b4f0383
Shubham Bansal <shubhambansal17@hotmail.com>
2018-04-24 04:24:52 +0530

    added the SearchFilter

--- myproject/article/viewsets.py
+++ myproject/article/viewsets.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 # @Author: shubhambansal
 # @Date:   2018-02-04 22:52:38
-# @Last Modified by:   shubhambansal
-# @Last Modified time: 2018-02-04 23:23:22
-from rest_framework import viewsets
+# @Last Modified by:   Shubham Bansal
+# @Last Modified time: 2018-04-24 03:37:47
+from rest_framework import viewsets, filters
 from .models import Article
 from .serializers import ArticleSerializer
+
 
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('article_id', 'article_heading', 'article_body')
